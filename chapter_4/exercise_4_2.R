9a4c1a176f9ce2990bee25da4c801cb80966d743
ilijavishinov-pc <ilijavishinovprofessional@gmail.com>
2022-05-07 14:20:18 +0200

    added requirements

--- chapter_4/exercise_4_2.R
+++
@@ -1,6 +0,0 @@
-library("readxl")
-df <- read_excel("C:\\Users\\ilija\\OneDrive\\Desktop\\rm\\exercises\\chapter_4\\data_4_2.xlsx")
-print(df)
-y <- df['y']
-x <- df['x']
-res.p=glm(y~x,family=poisson(link="identity"))
